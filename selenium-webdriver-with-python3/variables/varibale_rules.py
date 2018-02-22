e7959f9a9c0ce8225ed57a0dc25eb0918e8e8e84
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-02-22 11:17:04 +0300

    Variable rules

---
+++ selenium-webdriver-with-python3/variables/varibale_rules.py
@@ -0,0 +1,16 @@
+"""
+- no keywords
+- start with letter / underscorder
+- don't start with number
+- letters/numbers/underscore/
+- no special numbers
+"""
+
+import keyword
+
+print("Keywords:", keyword.kwlist)
+
+# and = 10
+# evariable$1 = 1
+a = b = c = 10
+print('a, b, c', a, b, c)
