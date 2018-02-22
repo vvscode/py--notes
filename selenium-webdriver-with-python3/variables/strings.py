49974cc2e8278e74eb65a290b7ea3a2a4cf6446b
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-02-22 11:46:53 +0300

    Some strings stuff

---
+++ selenium-webdriver-with-python3/variables/strings.py
@@ -0,0 +1,15 @@
+# in double / single quotes
+
+a = 'some string'
+b = "some other string"
+c = """some 
+"multiline" 
+string"""
+
+print(a)
+print(b)
+print(c)
+print("some \"sging \" with quotes")
+print('some other \'string\' with quotes')
+print("some other\
+      multiline")
