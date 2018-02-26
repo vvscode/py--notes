3c6a3b2a8491870247aa8e87c013d4c4104e430e
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-02-26 10:39:05 +0300

    List operations

--- selenium-webdriver-with-python3/data-types/lists.py
+++ selenium-webdriver-with-python3/data-types/lists.py
@@ -1,11 +1,32 @@
 cars = ['bmw', 'honda', 'audi']
 empty_list = []
-print(cars, empty_list)
+print(cars, empty_list) # ['bmw', 'honda', 'audi'] []
 
 x = 'test'
-print(list(x))
+print(list(x)) # ['t', 'e', 's', 't']
 
-print(cars[0])
+print(cars[0]) # bmw
 
 cars[1] = 'volga'
-print(cars)
+print(cars) #['bmw', 'volga', 'audi']
+
+print(len(cars)) # 3
+
+cars.append('Benz')
+print(cars) # ['bmw', 'volga', 'audi', 'Benz']
+
+cars.insert(1, 'jeep')
+print(cars) # ['bmw', 'jeep', 'volga', 'audi', 'Benz']
+
+print(cars.index('Benz')) # 4
+
+print(cars.pop()) # Benz
+print(cars) # ['bmw', 'jeep', 'volga', 'audi']
+
+cars.remove('jeep')
+print(cars) # ['bmw', 'volga', 'audi']
+
+print(cars[1:]) # ['volga', 'audi']
+
+cars.sort()
+print(cars) # ['audi', 'bmw', 'volga']
