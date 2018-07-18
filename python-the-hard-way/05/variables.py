0cf03cdd5a31319c22c1edee6a6dde8b3ae88ebf
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-07-18 10:40:01 +0300

    05 Hard Way - String formating

---
+++ python-the-hard-way/05/variables.py
@@ -0,0 +1,17 @@
+my_name = 'Vasiliy V'
+my_age = 35
+my_height = 186 # cms
+my_weight = 104 # kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'None'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} cms tall")
+print(f"He's {my_weight} kilos heavy")
+print("Actually that's quite heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair")
+print(f"His teeth are usually {my_teeth} depending on coffee")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, {my_weight} I get {total}")
