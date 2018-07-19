029f3ab7b86053b6f53000dad92443b9328d62c8
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-07-19 11:58:33 +0300

    10 Hard Way - Printing

--- python-the-hard-way/10/printing.py
+++ python-the-hard-way/10/printing.py
@@ -1,12 +1,14 @@
+x = 10
+
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line"
 backslash_cat = "I'm \\ a \\ cat"
-fat_cat = """
-I'll do a list:
+fat_cat = f'''
+I'll do a list {x}:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
 print(tabby_cat)
 print(persian_cat)
