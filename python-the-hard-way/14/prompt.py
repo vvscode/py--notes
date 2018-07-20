cbbf4891cd2b7d8c6699398dec4ecf8892a6d8b0
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-07-20 13:42:08 +0300

    14 Hard Way - Prompt

---
+++ python-the-hard-way/14/prompt.py
@@ -0,0 +1,25 @@
+from sys import argv
+
+script, user_name = argv
+
+prompt = "> "
+
+print(f"Hi {user_name}, I'm the {script} script")
+print("I'd like to ask you a few questions")
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print("What kind of computer do you have?")
+computer = input(prompt)
+
+print(f"""
+
+Ok, so you've said {likes} about liking me
+You live in {lives}. Not sure where that is
+
+And you hav a {computer} computer. Nice
+
+""")
