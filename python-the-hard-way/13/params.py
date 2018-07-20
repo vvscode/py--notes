0e199ac3770822250d150267987ffe4c51b48d57
Vasiliy Vanchuk @vvscode <v.vanchuk@tut.by>
2018-07-20 13:36:05 +0300

    13 Hard Way - Questions

---
+++ python-the-hard-way/13/params.py
@@ -0,0 +1,8 @@
+from sys import argv
+# python3 13/params.py one two 3
+script, first, second, third = argv
+
+print("The scriptis called:", script)
+print("Your first variable is:", first)
+print("YOur second variable is:", second)
+print("Your third variable is:", third)
