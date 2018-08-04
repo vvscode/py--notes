2561252cab0fa2d066ca6adb0f5c2e4c9c58cdef
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-08-04 09:57:35 +0400

    Bool operations

---
+++ python-the-hard-way/28/bool.py
@@ -0,0 +1,23 @@
+
+print('True and True\t\t\t',  True and True)
+print('')
+print('False and True\t\t\t',  False and True)
+print('')
+print('1 == 1 and 2 == 1\t\t\t',  1 == 1 and 2 == 1)
+print('')
+print('"test" == "test"\t\t\t',  "test" == "test")
+print('')
+print('1 == 1 or 2 != 1\t\t\t',  1 == 1 or 2 != 1)
+print('')
+print('True and 1 == 1\t\t\t',  True and 1 == 1)
+print('')
+print('False and 0 != 0 == "Cool Guy")\t\t\t',  False and 0 != 0 == "Cool Guy")
+print('')
+print('1 == 1 and (not ("testing" == 1 or 1 == 0))\t\t\t',
+      1 == 1 and (not ("testing" == 1 or 1 == 0)))
+print('')
+print('"chunky" == "bacon" and (not (3 == 4 or 3 == 3))\t\t\t',
+      "chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+print('')
+print('3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))\t\t\t',
+      3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
