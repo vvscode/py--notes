6006924fd6cd4916db32fc3aa941e697fbda9e03
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-09-01 16:51:41 +0400

    automation-testing-with-selenium-and-python 6.6

---
+++ automation-testing-with-selenium-and-python/lesson6_step6.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+
+link = "http://suninjuly.github.io/huge_form.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    elements = browser.find_elements(By.TAG_NAME, 'input')
+    for element in elements:
+       element.send_keys("Мой ответ")
+
+    button = browser.find_element_by_css_selector("button")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
